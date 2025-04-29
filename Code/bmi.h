ccf6de98423ca323c5371d9ff8ef2b9d8bac7edf
TalPeretz09 <talperetz999@gmail.com>
2025-04-29 18:35:43 +0200

    Added more comments

--- Code/bmi.h
+++ Code/bmi.h
@@ -2,6 +2,7 @@
 #ifndef BMI_H
 #define BMI_H
 
+//Declaration headers for each function in bmi.cpp
 double ounces2pounds(int ounces);
 double stones2pounds(int stones);
 double weight2kg(int stones, int pounds, int ounces);
--- Code/main.cpp
+++ Code/main.cpp
@@ -1,7 +1,7 @@
 int main(int argc, char *argv[])
 {
-    if (argc != 3) {
-        cerr << "Error: Please provide input and output filenames" << endl;
+    if (argc != 3) { //Checking for a third argument from the function
+        cerr << "ERR: Enter input and output file names" << endl;
         return 1;
     }
     process_data(argv[1], argv[2]);
